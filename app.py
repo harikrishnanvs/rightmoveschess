1af7908457591b99f31ab242b9657cd17c620ab7
Harikrishnan V S <harikriii@gmail.com>
2026-05-10 15:23:06 +0530

    c9-formsthankyouupdate

--- app.py
+++ app.py
@@ -1,26 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_mail import Mail, Message
 import os
 
 app = Flask(__name__)
 app.secret_key = "rightmoves_secret_key"
-
-# ==========================
-# CONFIGURE EMAIL SETTINGS
-# ==========================
-
-
-# MAIL CONFIGURATION
-
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')  
-
-mail = Mail(app)
-
 
 @app.route("/")
 def index():
@@ -38,12 +20,7 @@ def contact():
 
 @app.route("/thankyou")
 def thankyou():
-    return """
-    <h1 style='text-align:center;margin-top:100px;'>
-    ✅ Thank You!<br><br>
-    Our coaching team will contact you shortly.
-    </h1>
-    """
+    return render_template("thankyou.html") 
     
 if __name__ == "__main__":
     app.run(debug=True)
