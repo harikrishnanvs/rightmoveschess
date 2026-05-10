64de25b111f0bd3a00e0beee7c16988ff73211ab
Harikrishnan V S <harikriii@gmail.com>
2026-05-10 13:31:24 +0530

    c4-osimport

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')   #'xgzu cndz nygf xxzv'  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
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')  
 
 mail = Mail(app)
 
